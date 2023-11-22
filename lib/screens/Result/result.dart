4435d4a34424477d9624f364ff8222b8426d44cf
acepW <acepwahyu001@gmail.com>
2023-11-22 22:52:47 +0700

    coba kirim data

--- lib/screens/Result/result.dart
+++ lib/screens/Result/result.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:ringworm_detection/Components/snackBar.dart';
+import 'package:ringworm_detection/storage.dart';
+import 'package:tflite_v2/tflite_v2.dart';
 
 import 'package:url_launcher/url_launcher.dart';
+import 'package:uuid/uuid.dart';
 
 // ignore: must_be_immutable
 class Result extends StatefulWidget {
@@ -19,24 +25,26 @@ class Result extends StatefulWidget {
 
 class _ResultState extends State<Result> {
   List? result;
+  String? imageUrl;
+
   @override
   void initState() {
     super.initState();
-//     loadModelData().then((output) {
-// //after loading models, rebuild the UI.
-//       setState(() {
-//         result = null;
-//       });
-//     });
+    loadModelData().then((output) {
+//after loading models, rebuild the UI.
+      setState(() {
+        result = null;
+      });
+    });
   }
 
-//   loadModelData() async {
-// //tensorflow lite plugin loads models and labels.
-//     await Tflite.loadModel(
-//         model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  loadModelData() async {
+//tensorflow lite plugin loads models and labels.
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
 
-//     detectDisease();
-//   }
+    detectDisease();
+  }
 
   _openGoogleMaps() async {
     String url =
@@ -49,20 +57,43 @@ class _ResultState extends State<Result> {
     }
   }
 
-  // void detectDisease() async {
-  //   try {
-  //     result = await Tflite.runModelOnImage(
-  //       path: widget.imagefile.path,
-  //       numResults: 2,
-  //       threshold: 0.6,
-  //       imageMean: 127.5,
-  //       imageStd: 127.5,
-  //     );
-  //   } catch (e) {
-  //     // ignored, really.
-  //   }
-  //   setState(() {});
-  // }
+  void detectDisease() async {
+    try {
+      result = await Tflite.runModelOnImage(
+        path: widget.imagefile.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5,
+      );
+
+      addData();
+    } catch (e) {
+      // ignored, really.
+    }
+    setState(() {});
+  }
+
+  Future<void> addData() async {
+    final _uuid = const Uuid().v4();
+    try {
+      if (result![0]['label'] != null) {
+        imageUrl = await StorageMethods()
+            .uploadImageToStorage("penyakit", widget.imagefile, true);
+
+        await FirebaseFirestore.instance.collection('penyakit').doc(_uuid).set({
+          'uid': _uuid,
+          'img': imageUrl,
+          'accuracy': result![0]['confidence'],
+          'label': result![0]['label'],
+          'createdAt': DateTime.now(),
+        });
+        showSnackBar(context, "upload berhasil");
+      }
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
